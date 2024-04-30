6157ad824db35279f9d9cf57cb79a5c4f1c82c03
uparkalau <uladzimir.parkalau@gmail.com>
2024-04-30 01:18:25 -0700

    Styles added

--- wp-content/themes/missing-pets/functions.php
+++ wp-content/themes/missing-pets/functions.php
@@ -6,11 +6,17 @@ function missing_pets_directory_scripts() {
     // Enqueue Font Awesome CSS
     wp_enqueue_style('font-awesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
 
+    // Enqueue Custom CSS
+    wp_enqueue_style( 'missing-pets-style', get_template_directory_uri() . '/assets/css/custom.css' );
+
     // Enqueue Bootstrap JS
     wp_enqueue_script( 'bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), '5.3.3', true );
 
     // Enqueue jQuery UI (for Autocomplete)
     wp_enqueue_script( 'jquery-ui', 'https://code.jquery.com/ui/1.12.1/jquery-ui.js', array('jquery'), '1.12.1', true );
+
+    // Enqueue Custom JS
+    wp_enqueue_script( 'missing-pets-script', get_template_directory_uri() . '/assets/js/custom.js', array('jquery'), '1.0.0', true );
 
     // Enqueue theme stylesheet
     wp_enqueue_style( 'missing-pets-directory-style', get_stylesheet_uri() );
